6cb74936c4048a0c0ff476c90168f1f1191996a1
Aguirre, Lautaro Wenceslao <scrixsys@gmail.com>
2020-03-31 21:04:16 -0300

    First deploy
    Just contains the cover page

---
+++ precache-manifest.2f82bb06b2c86565a6e4bf99d18726e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bae3328d694b6bfd922f68cb1f88bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eadd4396ffad480f541b",
+    "url": "/static/css/main.408e94d2.chunk.css"
+  },
+  {
+    "revision": "f78bd366a52fbbee845c",
+    "url": "/static/js/2.21d9b05e.chunk.js"
+  },
+  {
+    "revision": "4aa5c1a985eabf61514d4a754c93962d",
+    "url": "/static/js/2.21d9b05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadd4396ffad480f541b",
+    "url": "/static/js/main.afade53d.chunk.js"
+  },
+  {
+    "revision": "034d92f3775c4bc83ff9",
+    "url": "/static/js/runtime-main.d9437dae.js"
+  }
+]);
